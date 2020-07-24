8b957bb0f7a7c32ea3165dc9a685850696da9d88
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-07-24 10:31:49 -0400

    -
    -

---
+++ be/knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/fishFriends2',
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+
+ 
+
+};
